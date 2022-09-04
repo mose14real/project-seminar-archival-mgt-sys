44a8f1e9c0faae00aefa0df0a19a54a3795bcec7
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-09-03 22:09:19 -0700

    setup complete, now working...

---
+++ resources/views/admin/admin.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
+    <!-- FONTAWESOME CDN -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Project & Seminar Archival Mgt Sys</title>
+</head>
+<body class="d-flex align-items-center justify-content-center text-primary" style="min-height:100vh">
+    <h1>Project and Seminar Management System (Admin) <i class="fa-solid fa-book-open-cover"></i> </h1>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
