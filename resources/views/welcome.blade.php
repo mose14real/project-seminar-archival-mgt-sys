82af8ca5ac2522ea57c26280adf2891cc81f8d9e
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-09-05 14:30:03 -0700

    add resources like svgs and so on, work on the utilities also

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,32 +6,35 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
-    <!-- FONTAWESOME CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- GENERAL STYLESHEET -->
-    <link rel="stylesheet" href="../css/app.css">
 
-<style>
-    *{
-        text-decoration:none !important;
-    }
-</style>
+    <link href="//db.onlinewebfonts.com/c/154ebfcef3ca74b92a56a5826ee85d74?family=Sharp+Sans+No1+Bold" rel="stylesheet" type="text/css"/>
+
+
+    <!-- FONTAWESOME CDN -->
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
+    <!-- GENERAL STYLESHEET -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <title>Project & Seminar Archival Mgt Sys</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg bg-light">
+
+<!-- NAVBAR SECTION STARTS HERE -->
+<nav class="navbar navbar-expand-lg">
   <div class="container">
-    <h1 class="navbar-brand font-bold" href="#">ProjectArch</h1>
+    <h1 class="navbar-brand font-bold text-white " href="#">ProjectArch</h1>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Project</a>
+          <a class="nav-link text-white active" aria-current="page" href="#">Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Seminar</a>
+          <a class="nav-link text-white " href="#">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link text-white " href="#">Seminars</a>
         </li>
       </ul>
       <!-- register and login button wrapper -->
@@ -46,7 +49,37 @@
     </div>
   </div>
 </nav>
-    <h1 class="text-center mt-5">Project and Seminar Management System <i class="fa-solid fa-book-open-cover"></i> </h1>
+
+<!-- HEADER SECTION STARTS HERE -->
+<header class="header-section container d-flex align-items-center justify-content-center flex-column">
+  <h1 class="text-center text-white">Project and Seminar Management System <i class="fas fa-book-open-cover"></i></h1>
+  <p class="mt-3 text-center text-white">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus ducimus recusandae voluptatem, commodi odio optio rem, aperiam odit veniam saepe amet quam atque expedita officia nihil reiciendis? Provident cumque molestiae libero minima quasi corporis tempora error necessitatibus officiis, doloribus deleniti ipsum mollitia cum ex blanditiis illum architecto sequi aut impedit, voluptate atque alias magnam expedita ab? Eligendi facere et autem, sunt possimus minus ex, iure tempore nam fuga harum ut officiis cupiditate vel cum
+  </p>
+  <div class="mx-auto mt-3 col-8">
+    <form>
+    <div class="row">
+      <div class="col-8">
+        <input type="search" class="form-control p-2" placeholder="Search Projects / Seminars" aria-label="First name">
+      </div>
+      <div class="col-4">
+        <button type="submit" class="btn btn-primary p-2 w-100">Search</button>
+      </div>
+    </div>
+    </form>
+  </div>
+
+  <!-- <img src="{{ asset('img/data-archival.svg') }}" alt="" class="data-archival-img"> -->
+</header>
+
+<div class="svgs">
+  <!-- <img src="{{ asset('img/data-archival.svg') }}" alt="" class=""> -->
+  <!-- <img src="" alt="">
+  <img src="" alt="">
+  <img src="" alt="">
+  <img src="" alt="">
+  <img src="" alt=""> -->
+</div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
