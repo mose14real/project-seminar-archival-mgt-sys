d77eb32512263062400d88cc320078cc4d5c7d12
Ariyibi-Baseet <ariyibi.baseet@gmail.com>
2022-09-04 19:05:48 -0700

    add raw bootstrap navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,13 +4,49 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- BOOTSTRAP -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <!-- FONTAWESOME CDN -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- GENERAL STYLESHEET -->
+    <link rel="stylesheet" href="../css/app.css">
+
+<style>
+    *{
+        text-decoration:none !important;
+    }
+</style>
     <title>Project & Seminar Archival Mgt Sys</title>
 </head>
-<body class="d-flex align-items-center justify-content-center text-primary" style="min-height:100vh">
-    <h1>Project and Seminar Management System <i class="fa-solid fa-book-open-cover"></i> </h1>
+<body>
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <h1 class="navbar-brand font-bold" href="#">ProjectArch</h1>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 mx-auto">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Project</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Seminar</a>
+        </li>
+      </ul>
+      <!-- register and login button wrapper -->
+      <div>
+        <a href="" class="">
+            <button class="btn btn-primary">Register</button>
+        </a>
+        <a href="" class="ml-3 mt-2">
+            <button class="btn btn-primary">Login</button>
+        </a>
+      </div>
+    </div>
+  </div>
+</nav>
+    <h1 class="text-center mt-5">Project and Seminar Management System <i class="fa-solid fa-book-open-cover"></i> </h1>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
